04a71b63c1e8ec3d46e0e667b9ac35eaba0396b3
Rezzy64 <rezzyze@gmail.com>
2022-12-02 09:02:50 +0100

    18.11.2022 Project

---
+++ src/F02_12_2022.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class F02_12_2022 {
+    public static void main (String[] args) {
+        Task3.main();
+    }
+}
+
+class Task3 {
+    public static void main() {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        for (int i=0; i<n; i++) {
+            System.out.println(fibrec(i)+" ");
+        }
+    }
+    public static int fibrec(int n) {
+        if (n<=0) {
+            return 0;
+        }
+        else if (n==1) {
+            return 1;
+        }
+        else {
+            return fibrec(n-1)+fibrec(n-2);
+        }
+    }
+}
